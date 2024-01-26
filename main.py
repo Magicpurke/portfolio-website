7b29840438cba63b9855f626a83394411759f58e
Miki Lovrenovic <lovrenovic.miki@gmail.com>
2024-01-26 14:44:35 +0100

    Added description on a new column

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, col2, = st.columns(2)
 
 with col1:
     st.image("images/photo.png", width=600)
@@ -13,3 +13,8 @@ with col2:
     Hi, I am Miki! I am a beginner Python programmer trying to make his way into the world of IT
     """
     st.info(content)
+
+text_content = """
+Below you can find some of the apps i have built in Python. Feel free to contact me!
+"""
+st.text(text_content)
