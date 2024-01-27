56759484b867c238c2990a247706d36a18ddfb4c
Miki Lovrenovic <lovrenovic.miki@gmail.com>
2024-01-27 18:40:17 +0100

    Add description, images and links

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -18,3 +19,21 @@ text_content = """
 Below you can find some of the apps i have built in Python. Feel free to contact me!
 """
 st.text(text_content)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("06 data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']}])")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']}])")
